e8eb590b97d7693be4294daff2877ba60bbe7721
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-31 14:36:55 -0700

    modify environment.js and results.js in order to implement apiKey

--- config/environment.js
+++ config/environment.js
@@ -38,9 +38,9 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
-
-  }
+  if (environment === 'development') {
+   ENV.myApiKey = process.env.apiKey;
+ }
 
   return ENV;
 };
